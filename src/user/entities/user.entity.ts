6baf980a33e2bd9f4c70da1038c6a19022c06ddc
Shewin Hudson <hudsonshewin@gmail.com>
2024-04-24 20:51:13 -0500

    Creacion de Recursos Users

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  phonenumber: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  adress: string;
+  
+  @Column({ default: true })
+  isActive: boolean;
+}
